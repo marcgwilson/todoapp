b27a8efc56170a86aeea6127147879b2caee39c5
UTU <utu@euphratesdata.com>
2019-11-02 00:06:12 -0700

    Update curl script.

--- curl.sh
+++ curl.sh
@@ -16,6 +16,10 @@ rfcdate() {
 	date "$@" -u +"%Y-%m-%dT%H:%M:%SZ"
 }
 
+todoid() {
+	echo $(echo "$@" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['id'];")
+}
+
 url="$ADDR/?state=todo&due:gt=$(rfcdate)&due:lt="$(rfcdate $(daysoffset '+10'))"&page=1&count=20"
 echo $url
 
@@ -28,7 +32,7 @@ result=$(curl -s -X POST $ADDR -d '{"desc":"My Todo", "due":"'$(rfcdate $(daysof
 
 echo $result
 
-id=$(echo $result | python -c "import json,sys;obj=json.load(sys.stdin);print obj['ID'];")
+id=$(echo $result | python -c "import json,sys;obj=json.load(sys.stdin);print obj['id'];")
 
 result=$(curl -s -X PATCH "$ADDR/$id/" -d '{"state": "in_progress"}' -H "Content-Type: application/json")
 
@@ -46,9 +50,23 @@ curl -X DELETE "$ADDR/$id/"
 
 curl -X GET "$ADDR/$id/"
 
+result=$(curl -s -X POST $ADDR -d '{"desc":"In progress TODO", "due":"'$(rfcdate $(daysoffset '+10'))'", "state": "in_progress"}' -H "Content-Type: application/json")
+
+ida=$(todoid $result)
+
+curl -X GET "$ADDR/$ida/"
+
+result=$(curl -s -X POST $ADDR -d '{"desc":"Done TODO", "due":"'$(rfcdate $(daysoffset '+10'))'", "state": "done"}' -H "Content-Type: application/json")
+
+idb=$(todoid $result)
+
+curl -X GET "$ADDR/$idb/"
+
 curl -X GET "$ADDR/?state=in_progress"
 curl -X GET "$ADDR/?state=done"
 curl -X GET "$ADDR/?state=todo"
+
+echo -e "\nERRORS\n"
 
 curl -s -X POST $ADDR -d '{"desc":"My Gabagoo", "due":"gabagoo", "state": "todo"}' -H "Content-Type: application/json"
 curl -s -X POST $ADDR -d '{"due":"gabagoo", "state": "todo"}' -H "Content-Type: application/json"
